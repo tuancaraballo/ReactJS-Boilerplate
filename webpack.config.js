73bcfac980913fab66e8283e305f5bc182905c70
Antuan Caraballo <tranc2606@gmail.com>
2017-07-18 06:09:48 -0700

    adding starting files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: [
+    './app/app.jsx',
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    alias: {
+      Main: __dirname + '/app/components/Main.jsx',
+    },
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ],
+  },
+
+  devtool: 'cheap-module-eval-source-map'
+};
